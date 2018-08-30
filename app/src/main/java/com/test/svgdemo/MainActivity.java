528a97f593aeef4ebcd897535a0abd9ef96e3cd0
zhumingwei <zhumingwei@wallstreetcn.com>
2018-08-30 18:55:57 +0800

    image

--- app/src/main/java/com/test/svgdemo/MainActivity.java
+++ app/src/main/java/com/test/svgdemo/MainActivity.java
@@ -1,17 +1,37 @@
 package com.test.svgdemo;
 
+import android.animation.ValueAnimator;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.test.svgdemo.widget.TemperatureGaugeView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TemperatureGaugeView view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TemperatureGaugeView view = findViewById(R.id.temperatureView);
-        view.setTemperature(0.52f);
+         view = findViewById(R.id.temperatureView);
+
+        view.setTemperature(0.5f);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ValueAnimator va = ValueAnimator.ofFloat(0.0f,1.0f).setDuration(20000);
+                va.setInterpolator(null);
+                va.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        Log.d("123",animation.getAnimatedValue().toString());
+                        view.setTemperature((Float) animation.getAnimatedValue());
+                    }
+                });
+                va.start();
+            }
+        },2000);
+
     }
 }
--- app/src/main/java/com/test/svgdemo/widget/TemperatureGaugeView.kt
+++ app/src/main/java/com/test/svgdemo/widget/TemperatureGaugeView.kt
@@ -1,16 +1,14 @@
 package com.test.svgdemo.widget
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.RectF
+import android.graphics.*
 import android.graphics.drawable.Drawable
 import android.support.annotation.FloatRange
 import android.support.graphics.drawable.VectorDrawableCompat
 import android.util.AttributeSet
 import android.view.View
 import com.test.svgdemo.R
+
 
 /**
  * @author zhumingwei
@@ -21,6 +19,10 @@ class TemperatureGaugeView @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
+
+    init {
+    }
+
     val liquidPaint: Paint by lazy {
         Paint().apply {
             color = Color.parseColor("#ff5252")
@@ -29,12 +31,13 @@ class TemperatureGaugeView @JvmOverloads constructor(
         }
     }
 
+
     val bgIconVD: Drawable? by lazy {
-        var dr:VectorDrawableCompat? = VectorDrawableCompat.create(resources, R.drawable.terpretation_icon,null);
+        var dr: VectorDrawableCompat? = VectorDrawableCompat.create(resources, R.drawable.terpretation_icon, null);
         dr?.setBounds(0, 0, icon_w, icon_h)
         dr
-
     }
+
     val icon_h: Int by lazy {
         height
     }
@@ -43,10 +46,10 @@ class TemperatureGaugeView @JvmOverloads constructor(
     }
 
     val temp_w: Int by lazy {
-        width/2
+        width / 2
     }
 
-    private var value: Float = 0f
+    private var value: Float = 0.0f
     fun setTemperature(@FloatRange(from = 0.0, to = 1.0) float: Float) {
         value = float
         invalidate()
@@ -57,8 +60,43 @@ class TemperatureGaugeView @JvmOverloads constructor(
         var centerx = width / 2
         var centery = height / 2
         canvas.drawOval(RectF(centerx.toFloat() - icon_w / 2f, centery.toFloat() + icon_h / 2f - icon_w, centerx.toFloat() + icon_w / 2f, centery.toFloat() + +icon_h / 2f), liquidPaint)
-        var t = (centery + icon_h / 2f - icon_w) - (icon_h - icon_w) * value
-        canvas.drawRect(RectF(centerx.toFloat() - temp_w / 2f, t, centerx.toFloat() + temp_w / 2f, centery + icon_h / 2f - icon_w+temp_w), liquidPaint)
+        drawWatter(canvas)
         bgIconVD?.draw(canvas)
+    }
+
+    fun drawWatter(canvas: Canvas) {
+        val centerx = width / 2
+        val centery = height / 2
+        var left = centerx.toFloat() - temp_w / 2f
+        val right = centerx.toFloat() + temp_w / 2f
+        var top = centery - icon_h / 2f
+        var bottom = centery + icon_h / 2f - icon_w + temp_w
+
+        val l: Float = width / 4f
+        val r = l / 2f
+        val wh = (centery + icon_h / 2f - icon_w) - (icon_h - icon_w) * value
+        val dh = top + r
+        if (wh >= dh) {
+            canvas.drawRect(RectF(left, wh, right, bottom), liquidPaint)
+        } else {
+            var rectF = RectF(left, top, right, top - l)
+            var jiaoarc = Math.asin(((dh - wh) / (r)).toDouble())
+            var cos = Math.cos(jiaoarc)
+            var w = r * cos
+            var jiao = jiaoarc * 180 / Math.PI
+            var p: Path = Path().apply {
+                moveTo(left, dh)
+                arcTo(rectF, 180.toFloat(), (180 + jiao).toFloat())
+                lineTo((centerx + w).toFloat(), dh)
+                arcTo(rectF, (360 - jiao).toFloat(), 360F)
+                lineTo(right, dh)
+                lineTo(right, bottom)
+                lineTo(left, bottom)
+                close()
+            }
+            canvas.drawPath(p, liquidPaint)
+        }
+
+
     }
 }
